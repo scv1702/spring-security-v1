4fc51766da481f64cee44370bc4c3e3b58b0f9c5
Changyu Shin <scv1702@naver.com>
2023-03-16 22:55:33 +0900

    feat: set spring security configuration

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // spring security filter를 filter chain에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()// 나머지 request는 모두 허가
+                .and()
+                .formLogin()
+                .loginPage("/login");
+    }
+}
--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -2,11 +2,42 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/users")
+    public String users() {
+        return "users";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join ";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원 가입 완료";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
     }
 }
